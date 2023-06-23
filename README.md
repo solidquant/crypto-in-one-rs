b8d5406baa7cb9aefe98415ebe93b59020769440
Solid Quant <solidquant@gmail.com>
2023-06-23 22:58:07 +0900

    fixed README and project name

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A simple CEX/DEX price aggregator.
 
 A WIP, but currently supports aggregating Binance, Bybit USD-M futures orderbook data.
+
+##### Inspired by:
+
+- [0xKitsune/bid_ask_service](https://github.com/0xKitsune/bid_ask_service)
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use tracing::{info, Level};
 use tracing_subscriber::{filter, prelude::*};
 
-use bid_ask_service_clone::exchanges::{binance, bybit};
+use crypto_in_one_rs::exchanges::{binance, bybit};
 
 #[tokio::main]
 async fn main() {
-    let filter = filter::Targets::new().with_target("bid_ask_service_clone", Level::INFO);
+    let filter = filter::Targets::new().with_target("crypto_in_one_rs", Level::INFO);
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(filter)
         .init();
 
     let symbols = vec!["BTCUSDT"];
-    let handle = bybit::bybit_orderbook_stream(symbols.clone());
+    let handle = binance::binance_orderbook_stream(symbols.clone());
     match handle.await {
         Ok(_) => info!("done"),
         Err(e) => info!("{e:?}"),
